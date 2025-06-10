cce5af2bc5056c4f72c0c77ad4ac2a473fd5f426
Kamal-Krushna-Sahu <sahukamalkrushna369@gmail.com>
2025-06-10 17:01:05 +0530

    worked on middlewares

--- videotube/backend/src/index.js
+++ videotube/backend/src/index.js
@@ -1,5 +1,15 @@
-import 'dotenv/config'
+import "dotenv/config";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
+const port = process.env.PORT || 3001;
 
-connectDB();
+connectDB() //DATABASE Connection
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MONGODB connection failed !`, err);
+  });
